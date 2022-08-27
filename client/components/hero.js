373626e3d273bfbb20da2ec222e7de072d78091d
engWafula <wafulaallan5@gmail.com>
2022-08-27 10:28:23 +0300

    more changes

--- client/components/hero.js
+++ client/components/hero.js
@@ -81,7 +81,10 @@ const Hero = () => {
           <div className='mt-3 sm:mt-0 sm:ml-3'>
             <div>
               <a className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10'>
-                Your Address: {shortenAddress(account)}
+                Your Address:{' '}
+                {account
+                  ? shortenAddress(account)
+                  : 'You No Metamask address connected'}
               </a>
             </div>
           </div>
