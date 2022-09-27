9bce773a8447405a8f3b67bca6b67084b0e37697
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2022-09-27 17:42:38 +0300

    Update new.js

--- client/pages/brand/new.js
+++ client/pages/brand/new.js
@@ -83,8 +83,9 @@ function NewBrandPage(props) {
           <div>
             <img
               className='mx-auto h-12 w-auto'
-              src='/logo.svg'
-              alt='BuySafe'
+              src='/logo.png'
+
+              alt='Tonfeera'
             />
             <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
               Create your own Brand
