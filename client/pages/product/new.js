44f7c97a30d2f705b034e057519221cde6ee56bc
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2022-10-14 07:39:03 +0100

    Update new.js

--- client/pages/product/new.js
+++ client/pages/product/new.js
@@ -158,9 +158,9 @@ function NewProductPage(props) {
         <div className='max-w-md w-full space-y-8'>
           <div>
             <img
-              className='mx-auto h-12 w-auto'
-              src='/logo.svg'
-              alt='BuySafe'
+              className='mx-auto h-20 w-auto'
+              src='/logo.png'
+              alt='Tonfeera'
             />
             <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
               Mint New Product Tokens
