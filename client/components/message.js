6e9fe7bfb4c4120a3d6942c2ac203c00753e76fc
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2022-11-04 07:18:47 +0000

    Update message.js

--- client/components/message.js
+++ client/components/message.js
@@ -3,7 +3,7 @@ const Message = ({ msg }) => {
     <div className='flex items-start justify-center py-12 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-md w-full space-y-8'>
         <div>
-          <img className='mx-auto h-12 w-auto' src='/logo.svg' alt='Workflow' />
+          <img className='mx-auto h-15 w-auto' src='/logo.png' alt='Workflow' />
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             {msg}
           </h2>
