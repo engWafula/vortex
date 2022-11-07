32d6dc8c2878677ca9bdb4f7429a49d29478bb83
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2022-11-07 07:17:19 +0000

    Update message.js

--- client/components/message.js
+++ client/components/message.js
@@ -1,3 +1,7 @@
+
+import Link from 'next/link';
+
+
 const Message = ({ msg }) => {
   return (
     <div className='flex items-start justify-center py-12 px-4 sm:px-6 lg:px-8'>
@@ -7,6 +11,12 @@ const Message = ({ msg }) => {
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             {msg}
           </h2>
+                            <Link
+                           href='/product/new'
+                    className='group relative w-full flex justify-center mt-2 py-5 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  >
+                    Start Minting Your Product
+                  </Link>
         </div>
       </div>
     </div>
