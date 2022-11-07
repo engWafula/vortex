b468f41a282271445dedd8b30367add2ee2a83f9
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2022-11-07 07:43:44 +0000

    Update message.js

--- client/components/message.js
+++ client/components/message.js
@@ -11,13 +11,6 @@ const Message = ({ msg }) => {
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             {msg}
           </h2>
-                 <div className='rounded-md shadow mt-10'>
-                    <Link href='/product/new'>
-                      <a className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'>
-                        Mint Your Product 
-                      </a>
-                    </Link>
-                  </div>
         </div>
       </div>
     </div>
