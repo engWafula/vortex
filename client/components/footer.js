87e10ef309162b6494c2b6eb86e66396c5752260
Wafula Allan  Isaac <73715320+engWafula@users.noreply.github.com>
2023-05-22 07:15:05 +0000

    adding new changes

--- client/components/footer.js
+++ client/components/footer.js
@@ -6,7 +6,9 @@ export default function footer() {
     <div className='w-full flex md:justify-center justify-between items-center flex-col p-4 bg-black'>
       <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
         <div className='flex flex-[0.5] justify-center items-center'>
-          <h1 className='text-2xl sm:text-2xl text-white'>Tonfeera</h1>
+          <h1 className='text-2xl sm:text-2xl text-white'>
+            Vortex Security System
+          </h1>
         </div>
         <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
           <Link href='/'>
@@ -39,14 +41,16 @@ export default function footer() {
         </p>
         <p className='text-white sm:text-lg  md:text-xl text-center font-medium mt-2'></p>
         <p className='text-white text-base  text-justify font-medium mt-2'>
-          GROUP CS22-19
+          {/* GROUP CS22-19 */}
         </p>
       </div>
 
       <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 ' />
 
       <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
-        <p className='text-white text-base  text-justify '>@Vortex Security System 2022</p>
+        <p className='text-white text-base  text-justify '>
+          @Vortex Security System 2022
+        </p>
         <p className='text-white text-base  text-justify '>
           All rights reserved
         </p>
